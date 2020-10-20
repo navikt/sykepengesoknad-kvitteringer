5ab9bbcc782175e7dda7f324cc5b38af1673e73c
Kent Daleng <kent.stefan.daleng@nav.no>
2020-10-20 09:33:12 +0200

    legger til fnr som metadata

--- src/main/kotlin/no/nav/syfo/bucket/api/BucketApi.kt
+++ src/main/kotlin/no/nav/syfo/bucket/api/BucketApi.kt
@@ -1,5 +1,7 @@
 package no.nav.syfo.bucket.api
 
+import com.google.cloud.storage.BlobId
+import com.google.cloud.storage.BlobInfo
 import com.google.cloud.storage.Bucket
 import com.google.cloud.storage.Storage
 import io.ktor.application.call
@@ -33,11 +35,11 @@ fun Route.setupBucketApi(storage: Storage, env: Environment) {
         val principal: JWTPrincipal = call.authentication.principal()!!
         val fnr = principal.payload.subject
         val bucket: Bucket? = storage.get(env.bucketName)
-        // TODO: Burde også sjekke om fnr er eier av reisetilskuddet
-        if (fnr != null && bucket != null) {
+        if (bucket != null) {
             call.respond(
-                bucket.list().iterateAll().joinToString(separator = "\n") { blob ->
-                    "${blob.name} (content-type: ${blob.contentType}, size: ${blob.size})"
+                bucket.list().iterateAll().filter{ it.metadata["fnr"] == fnr }
+                    .joinToString(separator = "\n") { blob ->
+                        "${blob.name} (content-type: ${blob.contentType}, size: ${blob.size})"
                 }
             )
         } else {
@@ -50,22 +52,25 @@ fun Route.setupBucketApi(storage: Storage, env: Environment) {
         val fnr = principal.payload.subject
 
         val bucket: Bucket? = storage.get(env.bucketName)
-        // TODO: Burde også sjekke om fnr er eier av reisetilskuddet
-        if (fnr != null && bucket != null) {
-            val blobName = call.parameters["blobName"]
+        if (bucket != null) {
+            val blobName = call.parameters["blobName"]!!
             val blob = bucket.get(blobName)
-            val kvittering = File(blobName)
-            kvittering.writeBytes(blob.getContent())
-            val kvitteringNavn = "kvittering-$blobName.${blob.contentType.split("/")[1]}"
-            call.response.header(
-                HttpHeaders.ContentDisposition,
-                ContentDisposition.Attachment.withParameter(
-                    ContentDisposition.Parameters.FileName,
-                    kvitteringNavn
-                ).toString()
-            )
-            call.respondBytes(kvittering.readBytes(), contentType = ContentType.parse(blob.contentType))
-            log.info("Returnerer $kvitteringNavn (content-type: ${blob.contentType})")
+            if (blob.metadata["fnr"] == fnr) {
+                val kvittering = File(blobName)
+                kvittering.writeBytes(blob.getContent())
+                val kvitteringNavn = "kvittering-$blobName.${blob.contentType.split("/")[1]}"
+                call.response.header(
+                    HttpHeaders.ContentDisposition,
+                    ContentDisposition.Attachment.withParameter(
+                        ContentDisposition.Parameters.FileName,
+                        kvitteringNavn
+                    ).toString()
+                )
+                call.respondBytes(kvittering.readBytes(), contentType = ContentType.parse(blob.contentType))
+                log.info("Returnerer $kvitteringNavn (content-type: ${blob.contentType})")
+            } else {
+                call.respond("fant ikke dokument")
+            }
         } else {
             call.respond("Bucket $env.bucketName does not exist.")
         }
@@ -76,19 +81,25 @@ fun Route.setupBucketApi(storage: Storage, env: Environment) {
         val fnr = principal.payload.subject
 
         val bucket: Bucket? = storage.get(env.bucketName)
-        // TODO: Burde også sjekke om fnr er eier av reisetilskuddet
-        if (fnr != null && bucket != null) {
+        val validator = VedleggValidator()
+
+        if (bucket != null) {
             val multipart = call.receiveMultipart()
             multipart.forEachPart { part ->
+                val blobNavn = UUID.randomUUID().toString()
                 if (part is PartData.FileItem) {
-                    val blobNavn = UUID.randomUUID().toString()
-                    val validator = VedleggValidator()
                     val fil = File(part.originalFileName!!)
                     part.streamProvider().use { input -> fil.outputStream().buffered().use { output -> input.copyToSuspend(output) } }
                     if (validator.valider(fil)) {
                         val type = validator.filtype(fil)
-                        val blob = fil.inputStream().buffered()
-                        bucket.create(blobNavn, blob, type.toString())
+                        val meta = mapOf("fnr" to fnr, "content-type" to type.toString())
+                        val blob = fil.inputStream().buffered().readBytes()
+
+                        val bId = BlobId.of(env.bucketName, blobNavn)
+                        val bInfo = BlobInfo.newBuilder(bId)
+                            .setMetadata(meta)
+                            .build()
+                        storage.create(bInfo, blob)
                         val vedleggRespons = VedleggRespons(blobNavn, "opprettet")
                         call.respond(TextContent(vedleggRespons.toJson(), ContentType.Application.Json.withCharset(Charsets.UTF_8), HttpStatusCode.Created))
                     } else {
