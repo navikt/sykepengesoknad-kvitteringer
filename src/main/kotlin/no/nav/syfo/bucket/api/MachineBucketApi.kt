333882e24a0c735365787aed07cc91f291a9e367
Kent D <kent.daleng@nav.no>
2021-01-13 09:27:07 +0100

    lager endepunkt for sletting av kvitteringer (#8)
    * lager endepunkt for sletting av kvitteringer
    
    * allow inbound

--- src/main/kotlin/no/nav/syfo/bucket/api/MachineBucketApi.kt
+++ src/main/kotlin/no/nav/syfo/bucket/api/MachineBucketApi.kt
@@ -2,32 +2,28 @@ package no.nav.syfo.bucket.api
 
 import com.google.cloud.storage.Bucket
 import com.google.cloud.storage.Storage
-import io.ktor.application.call
-import io.ktor.http.ContentDisposition
-import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
+import io.ktor.application.*
+import io.ktor.http.*
 import io.ktor.response.header
-import io.ktor.response.respond
 import io.ktor.response.respondBytes
 import io.ktor.routing.Route
 import io.ktor.routing.get
 import no.nav.syfo.Environment
 import no.nav.syfo.log
+import no.nav.syfo.models.jsonStatus
 import java.io.File
 
 fun Route.setupMachineBucketApi(storage: Storage, env: Environment) {
     get("/maskin/kvittering/{blobname}") {
-        val bucket: Bucket? = storage.get(env.bucketName)
-        if (bucket == null) {
-            call.respond("Bucket $env.bucketName does not exist.")
-            return@get
-        }
+        val bucket: Bucket = storage.get(env.bucketName) ?: throw RuntimeException("Bucket $env.bucketName finnes ikke")
 
         val blobName = call.parameters["blobName"]!!
         val blob = bucket.get(blobName)
         val kvittering = File(blobName)
         kvittering.writeBytes(blob.getContent())
         val kvitteringNavn = "$blobName.${blob.metadata?.get("content-type")!!.split("/")[1]}"
+
+        log.info("Returnerer $kvitteringNavn (content-type: ${blob.metadata?.get("content-type")})")
         call.response.header(
             HttpHeaders.ContentDisposition,
             ContentDisposition.Attachment.withParameter(
@@ -36,6 +32,16 @@ fun Route.setupMachineBucketApi(storage: Storage, env: Environment) {
             ).toString()
         )
         call.respondBytes(kvittering.readBytes(), contentType = ContentType.parse(blob.metadata?.get("content-type")!!))
-        log.info("Returnerer $kvitteringNavn (content-type: ${blob.metadata?.get("content-type")})")
+    }
+
+    get("/maskin/slett/{blobname}") {
+        val blobName = call.parameters["blobName"]!!
+        val ret = storage.delete(env.bucketName, blobName)
+        if (!ret) {
+            log.error("$blobName ble ikke funnet, og kan dermed ikke slettes")
+            call.jsonStatus(HttpStatusCode.NotFound, blobName, "ble ikke funnet, og kan dermed ikke slettes")
+            return@get
+        }
+        call.jsonStatus(id = blobName, message = "slettet")
     }
 }
--- src/main/kotlin/no/nav/syfo/models/VedleggRespons.kt
+++ src/main/kotlin/no/nav/syfo/models/VedleggRespons.kt
@@ -1,10 +1,24 @@
 package no.nav.syfo.models
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.response.*
 
 data class VedleggRespons(val id: String? = null, val melding: String)
 
 fun VedleggRespons.toJson(): String {
     val objectMapper = ObjectMapper()
     return objectMapper.writeValueAsString(this)
+}
+
+suspend inline fun ApplicationCall.jsonStatus(statusCode: HttpStatusCode = HttpStatusCode.OK, id: String? = null, message: String) {
+    respond(
+        statusCode,
+        TextContent(
+            VedleggRespons(id, message).toJson(),
+            ContentType.Application.Json.withCharset(Charsets.UTF_8)
+        )
+    )
 }
